f6aed6172b2a882995e9e395549d0b91e84ebf42
Mark Reynoso <mreynoso@spu.edu>
2018-03-06 13:34:39 -0800

    added link to readme, removed commented code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import Frame from '../components/frame.jsx';
 import '../styles/base.scss';
 
-// class Calc extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello World</h1>
-//       </div>
-//     )
-//   }
-// }
 
 ReactDOM.render(
   <Frame/>,
